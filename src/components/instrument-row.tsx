f03756648fcc6a4c8c75d6b1451abcdbd940bf9d
Cory West-Forbes <cory.westforbes@olioapps.com>
2020-05-01 04:10:20 -0700

    gen off color

--- src/components/instrument-row.tsx
+++ src/components/instrument-row.tsx
@@ -10,14 +10,19 @@ interface InstrumentRowProps {
 }
 
 class InstrumentRow extends Component<InstrumentRowProps> {
+  genOnOffColor = (beatOn: boolean): React.CSSProperties => {
+    return beatOn ? { opacity: 0.4 } : { opacity: 1 };
+  };
+
   renderRow = (): JSX.Element[] => {
     const { beats, title, selectBeatAction } = this.props;
 
     return beats.map((beat: Beat, i: number) => {
+      const opacity = this.genOnOffColor(beat.on);
       const backgroundColor: React.CSSProperties =
         i < 4 || (i > 7 && i < 12)
-          ? { backgroundColor: "#473d3d" }
-          : { backgroundColor: "#6b2b2b" };
+          ? { backgroundColor: "#473d3d", ...opacity }
+          : { backgroundColor: "#6b2b2b", ...opacity };
       return (
         <RowItem
           style={{
