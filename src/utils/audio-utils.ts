d9784ff391fd96f9b388cb9bc9403339d8fa76bc
Cory West-Forbes <cory.westforbes@olioapps.com>
2020-05-02 03:38:23 -0700

    setup audio utils

--- src/utils/audio-utils.ts
+++ src/utils/audio-utils.ts
@@ -1,9 +1,27 @@
-export const initAudio = (): AudioContext => {
+export const initAudio = (): AudioContext | undefined => {
   try {
-    const context = new AudioContext();
-
-    return context;
+    const AudioContext = window.AudioContext;
+    const audioContext = new AudioContext();
+    return audioContext;
   } catch (e) {
     console.log(`Web Audio API is not supported in this browser: ${e}`); // tslint:disable-line
   }
+};
+
+export const getFile = async (
+  audioContext: AudioContext,
+  filepath: string
+): Promise<AudioBuffer> => {
+  const response = await fetch(filepath);
+  const arrayBuffer = await response.arrayBuffer();
+  const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+  return audioBuffer;
+};
+
+export const setupSample = async (
+  audioContext: AudioContext,
+  filepath: string
+): Promise<AudioBuffer> => {
+  const sample = await getFile(audioContext, filepath);
+  return sample;
 };
