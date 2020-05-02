572cf3d3736cc3b55c3b27bd90b80d8f2bcb6cab
Cory West-Forbes <cory.westforbes@olioapps.com>
2020-05-02 03:46:06 -0700

    add playSound util

--- src/utils/audio-utils.ts
+++ src/utils/audio-utils.ts
@@ -1,4 +1,4 @@
-export const initAudio = (): AudioContext | undefined => {
+export const createAudioContext = (): AudioContext | undefined => {
   try {
     const AudioContext = window.AudioContext;
     const audioContext = new AudioContext();
@@ -24,4 +24,15 @@ export const setupSample = async (
 ): Promise<AudioBuffer> => {
   const sample = await getFile(audioContext, filepath);
   return sample;
+};
+
+export const playSample = (
+  audioContext: AudioContext,
+  audioBuffer: AudioBuffer
+): AudioBufferSourceNode => {
+  const sampleSource = audioContext.createBufferSource();
+  sampleSource.buffer = audioBuffer;
+  sampleSource.connect(audioContext.destination);
+  sampleSource.start();
+  return sampleSource;
 };
