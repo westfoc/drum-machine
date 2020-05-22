d1799d01a913c1026e0ba2ed3a8f199e91d95e36
Cory West-Forbes <cory.westforbes@olioapps.com>
2020-05-22 16:55:19 -0700

    add playsound action

--- src/redux/action-creators.ts
+++ src/redux/action-creators.ts
@@ -3,6 +3,7 @@ import { IsPlaying, SelectBeatParams } from "./core";
 export const SET_SELECT_BEAT = "SET_SELECT_BEAT";
 export const SET_TOGGLE_IS_PLAYING = "SET_TOGGLE_IS_PLAYING";
 export const HANDLE_START_PLAYBACK = "HANDLE_START_PLAYBACK";
+export const PLAY_SOUND = "PLAY_SOUND";
 
 interface SelectBeatAction {
   type: typeof SET_SELECT_BEAT;
@@ -11,6 +12,10 @@ interface SelectBeatAction {
 
 interface HandleStartPlaybackAction {
   type: typeof HANDLE_START_PLAYBACK;
+}
+
+interface PlaySoundAction {
+  type: typeof PLAY_SOUND;
 }
 
 export interface SetToggleIsPlaying {
@@ -39,7 +44,14 @@ export const handleStartPlayback = () => {
   };
 };
 
+export const playSound = (): PlaySoundAction => {
+  return {
+    type: PLAY_SOUND,
+  };
+};
+
 export type DrumMachineActionTypes =
   | SelectBeatAction
   | SetToggleIsPlaying
-  | HandleStartPlaybackAction;
+  | HandleStartPlaybackAction
+  | PlaySoundAction;
