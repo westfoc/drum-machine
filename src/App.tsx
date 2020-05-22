1a7853d8550e17cbdc2eda8da31dcf83dd55fb67
Cory West-Forbes <cory.westforbes@olioapps.com>
2020-05-22 16:55:25 -0700

    add ws

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import DrumFaceContainer from "./components/drum-face-container";
+
 function App() {
   return (
     <Provider store={store}>
