937c8c22048418e60d3e8e39849631b1937ba33e
Cory West-Forbes <cory.westforbes@olioapps.com>
2020-05-23 15:38:50 -0700

    remove last css file

--- src/components/drum-face-container.tsx
+++ src/components/drum-face-container.tsx
@@ -21,6 +21,12 @@ interface DrumFaceContainerProps {
 
 const appContainer: SerializedStyles = css`
   text-align: center;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 `;
 
 const appHeader: SerializedStyles = css`
